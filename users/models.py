7bb4da99660c56ce43aea7f31188091712109f52
mexic <mexic2284@mail.ru>
2025-02-10 21:42:16 +0300

    добавлен username у юзера и удален токен

--- users/models.py
+++ users/models.py
@@ -4,14 +4,13 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractUser):
+    username = models.CharField(max_length=255, null=True, blank=True, verbose_name="Username")
     email = models.EmailField(unique=True, verbose_name="Email")
     avatar = models.ImageField(
         upload_to="users/avatars/", blank=True, null=True, verbose_name="Аватар", help_text="Загрузите свой аватар"
     )
     phone_number = PhoneNumberField(blank=True, verbose_name="Телефон")
     country = models.CharField(blank=True, max_length=20, verbose_name="Страна проживания", help_text="Введите страну проживания")
-
-    token = models.CharField(max_length=100, verbose_name="Token", blank=True, null=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = [
@@ -24,4 +23,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
-    
