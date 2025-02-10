848256bbaa977a499438928b92d0761e231af52d
mexic <mexic2284@mail.ru>
2025-02-10 21:46:06 +0300

    в основных настройках добавлены пути до приложения lms, до встроенный моршрут для авторизации на основе сессий и кук и путь до приложения users для изменения пользователя

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('lms.urls', namespace='lms')),
+    path('users/', include('rest_framework.urls')),
+    path('user/', include('users.urls', namespace='user')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
