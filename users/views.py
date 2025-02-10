6682d0f48f6b80d90c141d5f00f1776e03e0c107
mexic <mexic2284@mail.ru>
2025-02-10 21:50:43 +0300

    удален прошлый по ошибке добавленный контроллер. добавлен контроллер изменения пользователя

--- users/views.py
+++ users/views.py
@@ -1,54 +1,12 @@
-import secrets
+from rest_framework import generics, permissions
+from django.contrib.auth import get_user_model
+from .serializers import UserProfileSerializer
 
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.mail import send_mail
-from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse, reverse_lazy
-from django.views.generic.edit import CreateView, UpdateView
-
-from config.settings import EMAIL_HOST_USER
-
-from .forms import UserEditForm, UserRegisterForm
-from .models import User
+User = get_user_model()
 
 
-class UserRegisterView(CreateView):
-    model = User
-    form_class = UserRegisterForm
-    template_name = "users/register.html"
-    success_url = reverse_lazy("users:login")
+class UserProfileUpdateView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
 
-    def form_valid(self, form):
-        user = form.save()  # получение пользователя
-        user.is_active = False
-        token = secrets.token_hex(16)
-        user.token = token
-        user.save()
-        host = self.request.get_host()
-        url = f"http://{host}/users/email-confirm/{token}/"
-        send_mail(
-
-            subject="Подтверждение почты",
-            message=f"Переход по ссылке для подтверждения почты {url}",
-            from_email=EMAIL_HOST_USER,
-            recipient_list=[user.email],
-        )
-
-        return super().form_valid(form)
-
-
-def email_verification(request, token):
-    user = get_object_or_404(User, token=token)
-    user.is_active = True
-    user.save()
-    return redirect(reverse("users:login"))
-
-
-class UserEditProfile(LoginRequiredMixin, UpdateView):
-    model = User
-    form_class = UserEditForm
-    template_name = "users/user_edit_form.html"
-    success_url = reverse_lazy("catalog:home")
-
-    def get_object(self, queryset=None):
+    def get_object(self):
         return self.request.user
