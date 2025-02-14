bafa46e46cdbe360f5d542390174dc73f4320c48
mexic <mexic2284@mail.ru>
2025-02-15 01:56:14 +0400

    сделана регистрация пользователя с jwt токеном

--- users/views.py
+++ users/views.py
@@ -1,12 +1,29 @@
 from rest_framework import generics, permissions
 from django.contrib.auth import get_user_model
-from .serializers import UserProfileSerializer
+from rest_framework.response import Response
+
+from .serializers import UserProfileSerializer, UserRegisterSerializer
 
 User = get_user_model()
 
 
+class UserRegisterAPIView(generics.CreateAPIView):
+    serializer_class = UserRegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserRegisterSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        tokens = serializer.save()
+        return Response({
+            'user': UserRegisterSerializer(tokens['user']).data,
+            'refresh': tokens['refresh'],
+            'access': tokens['access'],
+        })
+
+
 class UserProfileUpdateView(generics.RetrieveUpdateAPIView):
     serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self):
         return self.request.user
