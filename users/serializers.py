b96e8e209b284942cce3fccefdba278b2cdcfaff
mexic <mexic2284@mail.ru>
2025-02-15 02:01:27 +0400

    добавления сериализатора регистрации пользователя с осноными полями username, email, password. Остальные поля не обязательны. Добавлен jwt токен.  Добавлены сериализатор изменения профиля юзера

--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,38 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "avatar", "phone_number", "country"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+            avatar=validated_data.get("avatar", None),
+            phone_number=validated_data.get("phone_number", None),
+            country=validated_data.get("country", None),
+        )
+        refresh = RefreshToken.for_user(user)
+        return {
+            "user": user,
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'username', "avatar", 'phone_number', 'country']
-        read_only_fields = ['id',]
+        fields = ["id", "email", "username", "avatar", "phone_number", "country"]
+        read_only_fields = [
+            "id",
+        ]
