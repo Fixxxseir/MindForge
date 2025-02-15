f154d206d008253b86856a63186884b8350c2d51
mexic <mexic2284@mail.ru>
2025-02-16 00:56:59 +0400

    сделан сериализатор для модели Payment с выводом всех полей + текстовое представление полей payment_methods, payment_status. И title связанных объектов paid_course, paid_lesson. Так же добавлен сериализатор для детальной информации о пользователе + вложенный сериализор со всеми платежами пользователя.

--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from users.models import Payment
+
 User = get_user_model()
 
 
@@ -29,10 +31,50 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         }
 
 
+class PaymentSerializer(serializers.ModelSerializer):
+    payment_methods = serializers.SerializerMethodField()
+    payment_status = serializers.SerializerMethodField()
+    paid_course = serializers.SerializerMethodField()
+    paid_lesson = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Payment
+        fields = "__all__"
+
+    def get_payment_methods(self, obj):
+        return obj.get_payment_methods_display()
+
+    def get_payment_status(self, obj):
+        return obj.get_payment_status_display()
+
+    def get_paid_course(self, obj):
+        if obj.paid_course:
+            return obj.paid_course.title
+        return "Не приобретено"
+
+    def get_paid_lesson(self, obj):
+        if obj.paid_lesson:
+            return obj.paid_lesson.title
+        return "Не приобретено"
+
+
 class UserProfileSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = ["id", "email", "username", "avatar", "phone_number", "country"]
         read_only_fields = [
             "id",
+        ]
+
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "email", "username", "avatar", "phone_number", "country", "payments"]
+        read_only_fields = [
+            "id",
+            "payments",
         ]
