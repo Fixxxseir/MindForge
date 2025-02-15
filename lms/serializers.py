449c7f97736e114395dfdb6deec3ce5a047e4ff4
mexic <mexic2284@mail.ru>
2025-02-16 00:53:10 +0400

    в модель курса добавлено поле вывода количества всех уроков и поле информации по всем урокам курса

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,21 +3,32 @@ from rest_framework import serializers
 from lms.models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    lessons_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = "__all__"
-
-    def get_lessons_count(self, instance):
-        return instance.lessons.count()
-
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Course
+        fields = (
+            "id",
+            "title",
+            "description",
+            "image_preview",
+            "time_create",
+            "time_update",
+            "lessons_count",
+            "lessons",
+        )
+        read_only_fields = ("id",)
+
+    def get_lessons_count(self, instance):
+        return instance.lessons.count()
 
 
 # class CourseSerializer(serializers.Serializer):
