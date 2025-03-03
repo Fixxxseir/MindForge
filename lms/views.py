f1fed1edbf4324ba799780cdd962278a4f04af40
mexic <mexic2284@mail.ru>
2025-03-03 23:59:49 +0400

    добавлен контрллер подписки которая принимает пользователя и курс и если подписка есть то ее удаляет либо если подписки нет то добавляет

--- lms/views.py
+++ lms/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from users.permissions import IsModer, IsOwner
 
-from .models import Course, Lesson
+from .models import Course, Lesson, Subscription
+from .paginators import LmsPaginator
 from .serializers import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
+    pagination_class = LmsPaginator
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
     def get_queryset(self):
         if self.request.user.groups.filter(name="moders").exists():
-            return Course.objects.all()
-        return Course.objects.filter(owner=self.request.user)
+            return Course.objects.all().order_by("id")
+        return Course.objects.filter(owner=self.request.user).order_by("id")
 
     def get_permissions(self):
         if self.action == "create":
@@ -41,11 +46,12 @@ class LessonCreateAPIView(generics.CreateAPIView):
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     permission_classes = (IsAuthenticated, IsModer | IsOwner)
+    pagination_class = LmsPaginator
 
     def get_queryset(self):
         if self.request.user.groups.filter(name="moders").exists():
-            return Lesson.objects.all()
-        return Lesson.objects.filter(owner=self.request.user)
+            return Lesson.objects.all().order_by("id")
+        return Lesson.objects.filter(owner=self.request.user).order_by("id")
 
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
@@ -63,3 +69,22 @@ class LessonUpdateAPIView(generics.UpdateAPIView):
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
     permission_classes = (IsAuthenticated, ~IsModer | IsOwner)
+
+
+class SubscriptionAPIView(APIView):
+
+    def post(self, *args, **kwarg):
+        user = self.request.user
+        course_id = self.request.data.get("course_id")
+        course_item = get_object_or_404(Course, id=course_id)
+
+        sub_item = Subscription.objects.filter(owner=user, course=course_item)
+
+        if sub_item.exists():
+            sub_item.delete()
+            message = "подписка удалена"
+        else:
+            Subscription.objects.create(owner=user, course=course_item)
+            message = "подписка добавлена"
+
+        return Response({"message": message})
