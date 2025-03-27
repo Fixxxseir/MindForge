aee4d6954418798d880a2ad37129964081e8ba15
mexic <mexic2284@mail.ru>
2025-03-27 19:17:08 +0300

    фикс вызова deactivate_inactive_users

--- config/settings.py
+++ config/settings.py
@@ -221,7 +221,7 @@ CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
 CELERY_BEAT_SCHEDULE = {
     'deactivate_inactive_users': {
-        'task': 'courses.tasks.deactivate_inactive_users',
+        'task': 'users.tasks.deactivate_inactive_users',
         'schedule': timedelta(days=1),
     },
 }
