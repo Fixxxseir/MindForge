0d7a40baf534ed7c4aad7762605893b580e16dbb
mexic <mexic2284@mail.ru>
2025-04-16 17:41:38 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -98,7 +98,7 @@ if 'test' in sys.argv:
 else:
     DATABASES = {
         "default": {
-            "ENGINE": os.getenv("ENGINE"),
+            "ENGINE": 'django.db.backends.postgresql_psycopg2',
             "NAME": os.getenv("DB_NAME"),
             "USER": os.getenv("DB_USER"),
             "PASSWORD": os.getenv("DB_PASSWORD"),
