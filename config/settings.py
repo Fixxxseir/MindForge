0b6622884dff2977ea9a256d2f27fd13df87fee1
mexic <mexic2284@mail.ru>
2025-04-16 18:39:29 +0300

    fix 123456

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,7 @@ else:
             "USER": os.getenv("DB_USER", 'postgres'),
             "PASSWORD": os.getenv("DB_PASSWORD", 'postgres'),
             "HOST": os.getenv("DB_HOST", 'db'),
-            "PORT": os.getenv("DB_PORT"),
+            "PORT": os.getenv("DB_PORT", '5432'),
         }
     }
 
