29e0079dc4fce6fabd1274d33b51cf930bec5c79
mexic <mexic2284@mail.ru>
2025-04-19 19:08:44 +0300

    fix, add postgress test

--- config/settings.py
+++ config/settings.py
@@ -100,11 +100,11 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": 'django.db.backends.postgresql_psycopg2',
-            "NAME": os.getenv("DB_NAME", 'mindforge_db'),
-            "USER": os.getenv("DB_USER", 'postgres'),
-            "PASSWORD": os.getenv("DB_PASSWORD", 'postgres'),
-            "HOST": os.getenv("DB_HOST", 'localhost'),
-            "PORT": os.getenv("DB_PORT", '5432'),
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT"),
         }
     }
 
